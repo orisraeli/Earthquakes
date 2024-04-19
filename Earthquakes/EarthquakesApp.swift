588a91bee494d41bc46c093b2cf26d3b63c3549c
Or Israeli <isra.or@gmail.com>
2024-04-19 12:20:01 +0300

    Initial Commit

---
+++ Earthquakes/EarthquakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakesApp.swift
+//  Earthquakes
+//
+//  Created by Or Israeli on 19/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
