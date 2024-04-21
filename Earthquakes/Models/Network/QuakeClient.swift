fdf19a8a6477c990322e1286e2cb3aba7829cda1
Or Israeli <isra.or@gmail.com>
2024-04-21 19:04:24 +0300

    create QuakeClient

---
+++ Earthquakes/Models/Network/QuakeClient.swift
@@ -0,0 +1,31 @@
+//
+//  QuakeClient.swift
+//  Earthquakes
+//
+//  Created by Or Israeli on 21/04/2024.
+//
+
+import Foundation
+
+class QuakeClient {
+	var quakes: [Quake] {
+		get async throws {
+			let data = try await downloader.httpData(from: feedURL)
+			let decoded = try decoder.decode(GeoJSON.self, from: data)
+			return decoded.quakes
+		}
+	}
+	private lazy var decoder: JSONDecoder = {
+		let aDecoder = JSONDecoder()
+		aDecoder.dateDecodingStrategy = .millisecondsSince1970
+		return aDecoder
+	}()
+	
+	private let feedURL = URL(string: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson")!
+	
+	private let downloader: any HTTPDataDownloader
+	
+	init(downloader: any HTTPDataDownloader = URLSession.shared) {
+		self.downloader = downloader
+	}
+}
